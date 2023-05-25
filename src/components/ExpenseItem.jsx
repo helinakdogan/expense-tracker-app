b93df1c7bef112ddd4e6972ff2ff7491441029d8
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-25 05:25:30 +0300

    25.05.2023

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+function ExpenseItem(props) {
+    return (
+      <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
       </div>
-    </div>
-  )
-}
-
-export default ExpenseItem
+    );
+  }
+  
+  export default ExpenseItem;
