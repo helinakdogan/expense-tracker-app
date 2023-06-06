874d7ee02dfe78339b1e6b15c2391280814e8c4a
Helin Akdoğan <akdoganhelin@gmail.com>
2023-06-06 22:56:12 +0300

    06.06.2023

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -5,21 +5,13 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title)
-
-  //no need event
-  const clickHandler = () => {
-    setTitle('updated')
-    console.log(title)
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>change title</button>
+      </div> 
     </Card>
   );
 }
