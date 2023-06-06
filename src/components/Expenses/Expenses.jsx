fc65cfd276b8fdc7d58c8184c1dc53221104b976
Helin Akdoğan <akdoganhelin@gmail.com>
2023-06-06 16:32:20 +0300

    06.06.2023

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   return (
+<div>
+
     <Card className="expenses">
+    <ExpensesFilter/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +31,7 @@ const Expenses = (props) => {
         date={props.items[3].date}
       />
     </Card>
+    </div>
   );
 };
 
