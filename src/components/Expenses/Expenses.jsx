b92d35c09ff2dae5374530cd6f1f68f8f1f3b42d
Helin Akdoğan <akdoganhelin@gmail.com>
2023-06-07 23:31:09 +0300

    07.06.2023

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -19,6 +19,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
+
         {props.items.map((expense) => (
           <ExpenseItem
             key={expense.id}
