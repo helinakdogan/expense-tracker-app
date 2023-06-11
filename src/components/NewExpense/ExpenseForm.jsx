3a64f1dcc6d1759077d5cc69be27f017da1ee418
Helin Akdoğan <akdoganhelin@gmail.com>
2023-06-11 05:10:33 +0300

    11.06.2023

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -65,6 +65,7 @@ const ExpenseForm = (props) => {
             onChange={titleChangeHandler}
           />
         </div>
+
         <div className="new-expense__control">
           <label>Amount</label>
           <input
@@ -75,6 +76,7 @@ const ExpenseForm = (props) => {
             onChange={amountChangeHandler}
           />
         </div>
+
         <div className="new-expense__control">
           <label>Date</label>
           <input
@@ -86,10 +88,9 @@ const ExpenseForm = (props) => {
           />
         </div>
       </div>
+
       <div className="new-expense__actions">
-        <button type="button" onClick={props.onCancel}>
-          Cancel
-        </button>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
